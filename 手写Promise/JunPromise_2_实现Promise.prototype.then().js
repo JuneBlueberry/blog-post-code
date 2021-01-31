a9de6d7e5cf9f4c6858cd2684b56bd9c60b31259
JuneBlueberry <812391718@qq.com>
2021-01-31 18:15:08 +0800

    then方法修复
    then方法的pending状态也加入异步操作

--- 手写Promise/JunPromise_2_实现Promise.prototype.then().js
+++ 手写Promise/JunPromise_2_实现Promise.prototype.then().js
@@ -1,8 +1,8 @@
 /*
  * @Author: buleberry 
  * @Date: 2021-01-20 19:14:15 
- * @Last Modified by: buleberry
- * @Last Modified time: 2021-01-28 16:39:42
+ * @Last Modified by: blueberry
+ * @Last Modified time: 2021-01-31 18:07:00
  */
 
  /**
@@ -73,47 +73,57 @@
         //判断then方法是否有回调
         resolveCallback = resolveCallback ? resolveCallback : value => value
         rejectCallback = rejectCallback ? rejectCallback : reason => reason
-
+    
         let _promise = new JunPromise((resolve, reject) => {
             if(this.status == 'fulfilled') {
-                try {
-                    setTimeout(() => {
+                // 放入异步中，否则，此处_promise拿不到
+                // 将回调的结果传给下一个then方法
+                setTimeout(() => {
+                    try {
                         let resultPromise = resolveCallback(this.value)
-                        // return result
                         judgmentPromise(_promise, resultPromise, resolve, reject)
-                    }, 0);
-                } 
-                catch (error) {
-                    reject(error)
-                }
+                    } catch (error) {
+                        reject(error)
+                    }
+                }, 0);
             } else if(this.status == 'rejected') {
-                try {
-                    setTimeout(() => {
+                // 放入异步中，否则，此处_promise拿不到
+                // 将回调的结果传给下一个then方法
+                setTimeout(() => {
+                    try {
                         let resultPromise = rejectCallback(this.reason)
                         judgmentPromise(_promise, resultPromise, resolve, reject)
-                    }, 0);
-                } 
-                catch (error) {
-                    reject(error)
-                }
+                    } catch (error) {
+                        reject(error)
+                    }
+                }, 0);
             } else {
-                try {
-                    // 等待状态，记录回调函数，等待时间回调
-                    // this.resolveCallback.push(resolveCallback)
-                    // this.rejectCallback.push(rejectCallback)
-
-                    //then链式调用的时候，就需要把结果也返回给下一个then方法
-                    this.resolveCallback.push(() => {
-                        let resultPromise = resolveCallback(this.value)
-                        judgmentPromise(_promise, resultPromise, resolve, reject)
-                    })
-                    this.rejectCallback.push(() => {
-                        let resultPromise = rejectCallback(this.reason)
-                        judgmentPromise(_promise, resultPromise, resolve, reject)
-                    })
-                } catch (error) {
-                    reject(error)
-                }
+                // 等待状态，记录回调函数，等待时间回调
+                // this.resolveCallback.push(resolveCallback)
+                // this.rejectCallback.push(rejectCallback)
+    
+                // 等待状态，记录回调函数，等待事件回调
+                // then链式调用的时候，就需要把结果也返回给下一个then方法
+                this.resolveCallback.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let resultPromise = resolveCallback(this.value)
+                            judgmentPromise(_promise, resultPromise, resolve, reject)
+                        } catch (error) {
+                            reject(error)
+                        }
+                    }, 0);
+                })
+                this.rejectCallback.push(() => {
+                    setTimeout(() => {
+                        try {
+                            let resultPromise = rejectCallback(this.reason)
+                            judgmentPromise(_promise, resultPromise, resolve, reject)
+                        } catch (error) {
+                            reject(error)
+                        }
+                    }, 0);
+                })
             }
         })
         return _promise
